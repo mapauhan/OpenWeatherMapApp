17aee7e38bd018e7368c52d8704a1322a7edfd4b
Joseph Handschu <jhandschu@dkbyte.com>
2019-03-23 19:08:05 -0400

    Commit changes before merge into master

--- OpenWeatherMapApp/CurrentWeatherViewController.swift
+++ OpenWeatherMapApp/CurrentWeatherViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import Alamofire
 
 class CurrentWeatherViewController: UIViewController {
+    
+    @IBOutlet weak var lblTemp: UILabel!
+    
+    var selectedCountry: String?
+    var selectedCity: String?
+    var BASE_URL: String?
+    var API_KEY: String?
 
   
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//        let parameters: Parameters = [
+//            "country": self.selectedCountry,
+//            "city": self.selectedCity,
+//            "app_key": self.API_KEY
+//        ]
+        
+        let url = BASE_URL! //?? ""
+        let city = selectedCity! //?? ""
+        let apiKey = API_KEY! //?? ""
+        let country = selectedCountry! //?? ""
+        
+        let uri = "\(String(describing: url))?q=\(String(describing: city)),\(String(describing: country))&APIKEY=\(String(describing: apiKey))"
+        
+        
+        print(uri)
+        
+        AF.request(uri, method: HTTPMethod.post).responseJSON { response in
+            
+            print(response.result.value)
+            
+            let data = response.result.value as! [String: Any]
+            
+            let main = data["main"] as! [String:Any]
+            
+            let temp = main["temp"] as! NSNumber
+            
+            self.lblTemp.text = String(Double(truncating: temp))
+            print(temp)
+            
+        }
+        //a10dbba30194931c77982e032457ec48
     }
     
 
